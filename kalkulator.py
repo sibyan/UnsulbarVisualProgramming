e32845316e5e2e8da1fea807ddecc4298e555fd7
Muhammad Sibyan (D0216361) <101779571+sibyan@users.noreply.github.com>
2022-04-20 15:03:12 +0700

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,34 @@
+def tambah(a, b):
+    tambah = float(a) + float(b)
+    return tambah
+
+def kurang(a, b):
+    kurang = float (a) - float(b)
+    return kurang
+
+def kali(a, b):
+    kali = float(a) * float(b)
+    return kali
+
+def bagi(a, b):
+    bagi = float(a) / float(b)
+    return bagi
+
+while True:
+    print('\n\n\t\t========== KALKULATOR SEDERHANA ==========\n\n')
+    a = input('masukan bilangan 1: ')
+    b = input('masukan bilangan 2: ')
+    print('\n1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian\n5. Batal')
+    c = input('\npilih 1-5: ')
+    if c == '1':
+        print('hasilnya adalah: ', tambah(a, b))
+    elif c == '2':
+        print('hasilnya adalah: ', kurang(a, b))
+    elif c == '3':
+        print('hasilnya adalah: ', kali(a, b))  
+    elif c == '4':
+        print('hasilnya adalah: ', bagi(a, b))
+    elif c == '5':
+        break
+    else:
+        print("operasi tidak dikenal")
